bc021465333398358e832e802836dbf149d0fa03
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-11-27 09:09:25 -0300

    IP

--- Back-End/api_smart_city/api_smart_city/urls.py
+++ Back-End/api_smart_city/api_smart_city/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin;
+from django.urls import path, include;
+from django.http import HttpResponse;
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView;
 
 urlpatterns = [
     path('', lambda request: HttpResponse("PI em processo")),
